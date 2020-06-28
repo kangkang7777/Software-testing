07191589c8f3502f2b9fc3814866b2a39930ee2c
Huangjinx <1073130610@qq.com>
2020-06-28 20:55:36 +0800

    返回类型修改

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,14 +1,10 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.Calendar;
-import com.example.demo.service.CheckTriangle;
 import com.example.demo.service.FileReader;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/test")
@@ -18,22 +14,22 @@ public class TestController {
 
 
     @GetMapping(value = "/date/type1")
-    public List<String> testNextDate1() throws IOException {
+    public String testNextDate1() throws IOException {
         return fileReader.readNextDateFile1();
     }
 
     @GetMapping(value = "/date/type2")
-    public List<String> testNextDate2() throws IOException {
+    public String testNextDate2() throws IOException {
         return fileReader.readNextDateFile2();
     }
 
     @GetMapping(value = "/triangle/type1")
-    public List<String> testTriangle1() throws IOException {
+    public String testTriangle1() throws IOException {
         return fileReader.readTriangleFile1();
     }
 
     @GetMapping(value = "/triangle/type2")
-    public List<String> testTriangle2() throws IOException {
+    public String testTriangle2() throws IOException {
         return fileReader.readTriangleFile2();
     }
 }
--- demo/src/main/java/com/example/demo/service/FileReader.java
+++ demo/src/main/java/com/example/demo/service/FileReader.java
@@ -19,63 +19,68 @@ public class FileReader {
     @Resource
     private CheckTriangle checkTriangle;
 
-    public List<String> readNextDateFile1() throws IOException {
-        List<String> result = new ArrayList<>();
+    public String readNextDateFile1() throws IOException {
         XSSFWorkbook xssfWorkbook = new XSSFWorkbook(new FileInputStream(new File(System.getProperty("user.dir") + "/src/main/resources/data/nextDate.xlsx")));
         XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(0);
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 2; i < 21; i++){
             XSSFRow xssfRow = xssfSheet.getRow(i);
             int year = (int)xssfRow.getCell(1).getNumericCellValue();
             int month = (int)xssfRow.getCell(2).getNumericCellValue();
             int day = (int)xssfRow.getCell(3).getNumericCellValue();
-            result.add(calendar.getNextDate(year, month, day));
-            System.out.println(calendar.getNextDate(year, month, day));
+            String str = calendar.getNextDate(year, month, day) + "\n";
+            stringBuilder.append(str);
+            //System.out.println(calendar.getNextDate(year, month, day));
         }
-        return result;
+        return stringBuilder.toString();
     }
 
-    public List<String> readNextDateFile2() throws IOException {
-        List<String> result = new ArrayList<>();
+    public String readNextDateFile2() throws IOException {
         XSSFWorkbook xssfWorkbook = new XSSFWorkbook(new FileInputStream(new File(System.getProperty("user.dir") + "/src/main/resources/data/nextDate.xlsx")));
         XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(0);
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 24; i < 56; i++){
             XSSFRow xssfRow = xssfSheet.getRow(i);
             int year = (int)xssfRow.getCell(1).getNumericCellValue();
             int month = (int)xssfRow.getCell(2).getNumericCellValue();
             int day = (int)xssfRow.getCell(3).getNumericCellValue();
-            result.add(calendar.getNextDate(year, month, day));
-            System.out.println(calendar.getNextDate(year, month, day));
+            String str = calendar.getNextDate(year, month, day) + "\n";
+            stringBuilder.append(str);
+            //System.out.println(calendar.getNextDate(year, month, day));
         }
-        return result;
+        return stringBuilder.toString();
     }
 
-    public List<String> readTriangleFile1() throws IOException {
-        List<String> result = new ArrayList<>();
+    public String readTriangleFile1() throws IOException {
+
         XSSFWorkbook xssfWorkbook = new XSSFWorkbook(new FileInputStream(new File(System.getProperty("user.dir") + "/src/main/resources/data/triangle.xlsx")));
         XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(0);
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 2; i < 21; i++){
             XSSFRow xssfRow = xssfSheet.getRow(i);
             double a = xssfRow.getCell(0).getNumericCellValue();
             double b = xssfRow.getCell(1).getNumericCellValue();
             double c = xssfRow.getCell(2).getNumericCellValue();
-            result.add(checkTriangle.checkTriangle(a, b, c));
-            System.out.println(checkTriangle.checkTriangle(a, b, c));
+            String str = checkTriangle.checkTriangle(a, b, c) + "\n";
+            stringBuilder.append(str);
+            //System.out.println(checkTriangle.checkTriangle(a, b, c));
         }
-        return result;
+        return stringBuilder.toString();
     }
 
-    public List<String> readTriangleFile2() throws IOException {
-        List<String> result = new ArrayList<>();
+    public String readTriangleFile2() throws IOException {
         XSSFWorkbook xssfWorkbook = new XSSFWorkbook(new FileInputStream(new File(System.getProperty("user.dir") + "/src/main/resources/data/triangle.xlsx")));
         XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(0);
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 24; i < 37; i++){
             XSSFRow xssfRow = xssfSheet.getRow(i);
             double a = xssfRow.getCell(1).getNumericCellValue();
             double b = xssfRow.getCell(2).getNumericCellValue();
             double c = xssfRow.getCell(3).getNumericCellValue();
-            result.add(checkTriangle.checkTriangle(a, b, c));
-            System.out.println(checkTriangle.checkTriangle(a, b, c));
+            String str = checkTriangle.checkTriangle(a, b, c) + "\n";
+            stringBuilder.append(str);
+            //System.out.println(checkTriangle.checkTriangle(a, b, c));
         }
-        return result;
+        return stringBuilder.toString();
     }
 }

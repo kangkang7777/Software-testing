afba7a086e4afe0d2774f5b3d8bcaf832cc8c415
Krisskr <546628725@qq.com>
2020-06-29 19:45:30 +0800

    带缺陷的单元测试完成

--- xuezhi-backend/check_service/src/main/java/com/xuezhi/check_service/adapter/out/ReportRepositor.java
+++ xuezhi-backend/check_service/src/main/java/com/xuezhi/check_service/adapter/out/ReportRepositor.java
@@ -1,6 +1,7 @@
 package com.xuezhi.check_service.adapter.out;
 
 import com.xuezhi.check_service.domain.entity.Report;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ReportRepositor extends MongoRepository<Report, String> {
@@ -8,4 +9,5 @@ public interface ReportRepositor extends MongoRepository<Report, String> {
     Report findReportById(String id);
     Report findReportByQuestionId(String questionId);
     Report findReportByQuestionIdAndAuthorId(String questionId, String authorId);
+
 }
